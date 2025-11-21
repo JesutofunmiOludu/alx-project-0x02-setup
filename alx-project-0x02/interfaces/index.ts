75b27ca61eced38fe05e5286d2d58da27f3827b5
LaughterOludu <99695248+LaughterOludu@users.noreply.github.com>
2025-11-21 10:19:53 +0100

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -21,4 +21,10 @@ export interface UserProps {
   name: string;
   username: string;
   email: string;
+  address?: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
 }
